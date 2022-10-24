3f957e0258d91a191754ecbd6545f9b454d381a3
mayela101 <113002207+mayela101@users.noreply.github.com>
2022-10-23 23:28:51 -0700

    First push

---
+++ MagicSquareCmnts.c
@@ -0,0 +1,18 @@
+
+//declare arrays and all ints
+
+//enering order
+
+//eneting values
+
+//print square with values
+
+//for loops to show all summations
+
+//diagonal
+
+//row
+
+//column
+
+//output results
